12463cf4fe036e59777cf28aaa174622e28d6b53
peterhangg <peterhanggg@gmail.com>
2019-10-07 23:00:58 +0000

    implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  console.log("Testing......");
+  $(".tweet-box").on("input", function() {
+    // console.log("key presses!");
+    console.log(this);
+    console.log($(this));
+    let length = $(this).val().length;
+    let charCounter = 140 - length;
+    $(".counter").text(charCounter);
+    if(charCounter < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "green");
+    }
+  });
+});
