945d478c74967e2b47de932b8578c9efb08fb28a
peterhangg <peterhanggg@gmail.com>
2019-10-08 19:55:21 +0000

    Added tweets dynamically with a loop

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,63 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $( document ).ready(function() {
   const createTweetElement = (obj) => {
-    let html = `<article>
+    let html = 
+    `<article>
       <header>
-      <div class="head-name">
-        <img src="${obj.user.avatars}"/>
-        <span>${obj.user.name}</span>
-      </div>
-      <h5 class="head-username">${obj.user.handle}</h5>
+        <div class="head-name">
+          <img src="${obj.user.avatars}"/>
+          <span>${obj.user.name}</span>
+        </div>
+        <h5 class="head-username">${obj.user.handle}</h5>
       </header>
-      <div class="tweet">
+      <div class="tweet-text">
       ${obj.content.text}
       </div>
       <footer>
-      <span>${new Date(obj.created_at).toDateString()}</span>
-      <div class="icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
+        <span>${new Date(obj.created_at).toDateString()}</span>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
       </footer>
     </article>`;
     return $(html);
   };
   
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const renderTweets = function(tweets) {
+    for(let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    };
+    
+  };
+
+  renderTweets(data);
 });
 
